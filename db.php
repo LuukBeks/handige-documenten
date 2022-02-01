3b08e2329ad11134eef9d814781c516eb4d4e100
sander-hash <69810469+sander-hash@users.noreply.github.com>
2022-02-01 16:05:01 +0100

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$user="root";
+$pass="";
+try {
+    $db = new PDO('mysql:host=localhost;dbname=bioscoop', $user, $pass);
+} catch (PDOException $e) {
+    error_log($e->getMessage(), 3, "../backend/error_log.txt");
+    die('Er is een probleem met de database verbinding');
+    
+    
+}
+?>
